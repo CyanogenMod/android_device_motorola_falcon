09a4710d46cb1ca50496e48d918d28778e0d2fdd
dhacker29 <dhackerdvm@gmail.com>
2014-03-21 01:58:27 -0400

    Move Network settings to msm8226-common
    Change-Id: I2c3472ab284375d9f6da9dadd1f1573fb47490e8

--- device_falcon.mk
+++ device_falcon.mk
@@ -30,13 +30,5 @@ PRODUCT_LOCALES += xhdpi
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
-# CDMA, GSM/WCDMA
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.telephony.default_network=5 \
-	telephony.lteOnCdmaDevice=1 \
-	persist.radio.mode_pref_nv10=1 \
-	persist.radio.no_wait_for_card=1 \
-	persist.radio.dfr_mode_set=1
-
 $(call inherit-product, device/motorola/msm8226-common/keylayout/keylayout.mk)
 $(call inherit-product, vendor/motorola/falcon/falcon-vendor.mk)

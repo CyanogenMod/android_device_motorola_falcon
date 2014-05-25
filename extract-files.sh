5ccf0259c021fa839a2ac17441fd038d0f852da1
dhacker29 <dhackerdvm@gmail.com>
2014-05-25 01:28:06 -0400

    extract-files: update for new setup-makefiles
    Change-Id: Id8779f2e01986a8a601b8dfca3c18eb9fb0e2143

--- extract-files.sh
+++ extract-files.sh
@@ -2,5 +2,9 @@
 
 export VENDOR=motorola
 export DEVICE=falcon
+# Comment these out if there is no common device tree for this device
+export COMMON_DEVICE=msm8226-common
+export BOARD_VENDOR=motorola-qcom
+export TARGET_BOARD_PLATFORM=msm8226
 
-../msm8226-common/extract-files.sh $@
+../$COMMON_DEVICE/extract-files.sh $@

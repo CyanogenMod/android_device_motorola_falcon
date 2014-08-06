bad9c7275752ea514bb277632b492c48f1915224
Ethan Chen <intervigil@gmail.com>
2014-08-06 15:36:31 -0700

    falcon: Import init scripts from qcom/msm8226 common
    Change-Id: Iea46d20b7d4710d31717faa1e267d379b3fadd9f

---
+++ rootdir/etc/mount_pds.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+export PATH=/system/xbin:$PATH
+
+if [ ! -f /cache/pds-CM10.img ]
+then
+    #make a copy of pds in /cache
+    /system/xbin/dd if=/dev/block/platform/msm_sdcc.1/by-name/pds of=/cache/pds-CM10.img
+    echo "Backed up PDS"
+fi
+
+#mount the fake pds
+/system/xbin/losetup /dev/block/loop0 /cache/pds-CM10.img
+/system/xbin/busybox mount -o rw /dev/block/loop0 /pds
+/system/bin/restorecon -R /pds
+echo "Mounted PDS"

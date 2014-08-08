eed1e864a25fc4e7fd3b963430d0aa7ff1f18348
Ethan Chen <intervigil@gmail.com>
2014-08-08 14:10:59 -0700

    falcon: Remove GPS HAL
    Change-Id: Id5d1dd273384d108500969e4686a189fb599d875

--- device.mk
+++ device.mk
@@ -126,17 +126,6 @@ PRODUCT_PACKAGES += \
     libqcomfm_jni \
     qcom.fmradio
 
-# GPS
-PRODUCT_PACKAGES += \
-    gps.msm8226
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
-
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config

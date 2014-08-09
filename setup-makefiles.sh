274482ebf16322c4b045306598fb7a8c471a0fac
Ethan Chen <intervigil@gmail.com>
2014-08-08 20:37:55 -0700

    falcon: Re-add TimeService apk
    Change-Id: I92497249b4cda3eaa87056146a9ade9f1f08436e

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -67,6 +67,7 @@ done
 # Pick up overlay for features that depend on non-open-source files
 PRODUCT_PACKAGES += \\
     com.qualcomm.location \\
+    TimeService \\
     qcrilmsgtunnel \\
     imslibrary \\
     qcnvitems \\
@@ -120,6 +121,16 @@ include \$(CLEAR_VARS)
 LOCAL_MODULE := com.qualcomm.location
 LOCAL_MODULE_OWNER := $VENDOR
 LOCAL_SRC_FILES := proprietary/app/com.qualcomm.location.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := TimeService
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/app/TimeService.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS

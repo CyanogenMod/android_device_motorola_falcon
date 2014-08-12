58b4fce3d1c2ff8f919c85815e1ac7aefe448f3a
Ethan Chen <intervigil@gmail.com>
2014-08-12 12:45:46 -0700

    falcon: Add custom ACDB IDs
    Change-Id: I389b2f49d899503671f242273d15c2216fb920bb

--- device.mk
+++ device.mk
@@ -76,6 +76,7 @@ PRODUCT_PACKAGES += \
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/configs/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
 

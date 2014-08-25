ce5fa45ac824f7c385404231c70cc5c964c4108e
Alberto97 <albertop2197@gmail.com>
2014-08-25 21:14:00 +0200

    Include msm8226-common Vendor
    Big thanksssssssssssssssssssssssssssssssssssssss
    @slv96 @slv96 @slv96 @slv96
    
    Signed-off-by: Alberto97 <albertop2197@gmail.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, vendor/motorola/falcon/falcon-vendor.mk)
 
+$(call inherit-product, vendor/motorola/msm8226-common/msm8226-common-vendor.mk)
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 

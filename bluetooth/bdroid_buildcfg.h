c6b66e9f6da6d3c878ba3d78d1d0fa9606f2b480
Konsta <konsta09@gmail.com>
2014-10-28 22:57:27 +0200

    msm8226-common: bluetooth: bring build config in line with CAF
    * remove redundant flags
    
    Change-Id: I3599e0be9511277a3eb6ae65e1033902886247e7

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2012 The CyanogenMod Project <http://www.cyanogenmod.org>
+ * Copyright (C) 2014 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,8 +19,12 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "Moto G"
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BLUETOOTH_QCOM_SW TRUE
-#define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
-#define BTC_INCLUDED TRUE
+// Enables Interleave scan
+#define BTA_HOST_INTERLEAVE_SEARCH  TRUE
+// Disables read remote device feature
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
+#define MAX_L2CAP_CHANNELS    14
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD  TRUE
 #endif

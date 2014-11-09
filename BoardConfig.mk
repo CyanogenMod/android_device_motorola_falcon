aea7b2e517ef796c50fc42b0ea2acdafc0fea2fc
Alberto97 <albertop2197@gmail.com>
2014-11-09 18:03:03 +0100

    Include more fonts
    Change-Id: I6e088d805c402da3355a3967411ee3d8d0382fc0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,3 +151,6 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
 WIFI_DRIVER_MODULE_NAME := "wlan"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true

ef95a0196636e8ec608a2b7b27275f5e298c83a3
Alberto97 <albertop2197@gmail.com>
2014-11-12 17:22:52 +0100

    Add others common codecs
    Signed-off-by: Alberto97 <albertop2197@gmail.com>

--- device.mk
+++ device.mk
@@ -150,6 +150,8 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml

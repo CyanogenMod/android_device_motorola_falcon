0d5d0c46a4070483649080b9db8ed0f44b9e2088
Alberto97 <albertop2197@gmail.com>
2014-11-16 18:19:33 +0100

    More CM

--- cm.mk
+++ cm.mk
@@ -1,7 +1,35 @@
-$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from falcon device
+$(call inherit-product, device/motorola/falcon/device.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Inherit APNs list
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
 PRODUCT_RELEASE_NAME := MOTO G
+PRODUCT_DEVICE := falcon
 PRODUCT_NAME := cm_falcon
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := falcon
+PRODUCT_MANUFACTURER := motorola

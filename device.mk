a5674c4567b48cdd4b79127679056ba8e85d2bf7
LuK1337 <priv.luk@gmail.com>
2015-01-24 00:39:33 +0100

    falcon: Move mixer_paths.xml to device
    Titan has to use own mixer_paths.xml (stereo speakers), so move mixer_paths.xml here.
    
    Change-Id: I5aad6c75707ab7b546094d66999dc5489d3461d5

--- device.mk
+++ device.mk
@@ -20,6 +20,10 @@ $(call inherit-product, vendor/motorola/falcon/falcon-vendor.mk)
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom

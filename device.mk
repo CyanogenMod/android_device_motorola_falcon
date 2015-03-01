62741fad8fcf1426ab09344d76efa49c47df9153
Michael Stucki <mundaun@gmx.ch>
2015-03-01 01:26:55 +0100

    falcon: Sync with peregrine
    Change-Id: I92a87711cd5757c69cf16642a581a0feaf84761c

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 # call the proprietary setup
-$(call inherit-product, vendor/motorola/falcon/falcon-vendor.mk)
+$(call inherit-product-if-exists, vendor/motorola/falcon/falcon-vendor.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay

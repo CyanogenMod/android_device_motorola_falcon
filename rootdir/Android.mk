811b14325996cbaa1ebf07d0bce5702d5e7b33f5
Brandon McAnsh <brandon.mcansh@gmail.com>
2015-07-24 16:28:10 -0400

    falcon: Add device specific hooks to ramdisk
    Change-Id: I53c81ccb4906081a58f99d49fbfc49476472f0fb
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,3 +9,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.device.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.device.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+

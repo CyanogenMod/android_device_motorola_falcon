936401a6080f1eefbd0707a357b8668769c48021
Luca Stefani <luca.stefani.ge1@gmail.com>
2015-09-16 11:23:29 -0700

    falcon: update boost fingerprint
    Change-Id: I1df2064c8ccb0e415a710859843ad74b4dfa37b7

--- init/init_falcon.c
+++ init/init_falcon.c
@@ -104,8 +104,8 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         } else {
             /* xt1031 */
             property_set("ro.product.device", "falcon_cdma");
-            property_set("ro.build.description", "falcon_boost-user 4.4.2 KXB20.9-1.10-1.18 18 release-keys");
-            property_set("ro.build.fingerprint", "motorola/falcon_boost/falcon_cdma:4.4.2/KXB20.9-1.10-1.18/18:user/release-keys");
+            property_set("ro.build.description", "falcon_boost_edf-user 5.0.2 LXB22.99-18 9 intcfg,release-keys");
+            property_set("ro.build.fingerprint", "motorola/falcon_boost_edf/falcon_cdma:5.0.2/LXB22.99-18/9:user/intcfg,release-keys");
             property_set("ro.build.product", "falcon_cdma");
             property_set("persist.radio.multisim.config", "");
             property_set("ro.mot.build.customerid", "sprint");

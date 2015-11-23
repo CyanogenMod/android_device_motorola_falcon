4b82ed8036cfa16b20c9169830242acd48ef0876
LuK1337 <priv.luk@gmail.com>
2015-11-23 14:58:53 -0500

    falcon: init: Specify ro.telephony.ril.config
    * We need that for sim detection on MSIM variants.
    
    Change-Id: I2b8f521c664f379e76f3075b64d2af4985806736

--- init/init_falcon.cpp
+++ init/init_falcon.cpp
@@ -130,6 +130,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.product", "falcon_umtsds");
         property_set("ro.mot.build.customerid", "RETBR");
         property_set("ro.telephony.default_network", "0,1");
+        property_set("ro.telephony.ril.config", "simactivation");
         property_set("persist.radio.multisim.config", "dsds");
         property_set("persist.radio.dont_use_dsd", "true");
         property_set("persist.radio.plmn_name_cmp", "1");

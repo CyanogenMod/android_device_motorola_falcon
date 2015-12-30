9212a63c396ceed4d59f2f22a21c7db65a1e3abe
Gabriele M <moto.falcon.git@gmail.com>
2015-12-30 14:04:22 +0100

    falcon: Add button jack keylayout
    Change-Id: I766fc6ccf6763285a11704661436b52fa82336e2

--- device.mk
+++ device.mk
@@ -32,5 +32,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/msm8226-tapan9302-snd-card_Button_Jack.kl:system/usr/keylayout/msm8226-tapan9302-snd-card_Button_Jack.kl
+
 # Inherit from msm8226-common
 $(call inherit-product, device/motorola/msm8226-common/msm8226.mk)

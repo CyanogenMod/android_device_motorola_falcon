bc14ce2b340f29bc3d97b75bb4e01bf09394ee8d
Gabriele M <moto.falcon.git@gmail.com>
2016-01-19 22:00:19 +0100

    falcon: init: Fixup CDMA carrier detection
    falcon_3.img.gz exists in many variants and is always a symlink to
    falcon_verizon_3.img.gz, so it's not enough to determine the carrier.
    Moreover, there are more carriers than just Verizon and Boost Mobile.
    Also, the numeric for Boost Mobile is different from the one used
    in factory images.
    
     - If ro.boot.carrier=vzw, set the properties for Verizon.
       This property is not normally available and can be set using the
       following fastboot command: fastboot oem config carrier vzw
    
     - If /pds/public/usc/ exists, set the properties for U.S. Cellular.
       This directory is created by init.usc.rc, available only in USC
       factory images.
    
     - If /persist/prop/ro_cust.prop exists, set the properties for
       Sprint MVNOs. This file is most likely available for Sprint MVNO.
    
    Other carriers are not supported. However, this should not cause
    issues with mobile data, calls and SMS.
    
    Change-Id: Ia789338cad410603a225c95523e713c28cf72d5e

--- init/init_falcon.cpp
+++ init/init_falcon.cpp
@@ -29,6 +29,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
@@ -37,12 +38,34 @@
 
 #include "init_msm.h"
 
+enum supported_carrier {
+    UNKNOWN = -1,
+    VERIZON,
+    SPRINT,
+    USC,
+};
+
+static enum supported_carrier detect_carrier(void)
+{
+    char carrier[PROP_VALUE_MAX];
+
+    property_get("ro.boot.carrier", carrier);
+    if (ISMATCH(carrier, "vzw")) {
+        return VERIZON;
+    } else if (access("/pds/public/usc", F_OK) != -1) {
+        return USC;
+    } else if (access("/persist/prop/ro_cust.prop", F_OK) != -1) {
+        return SPRINT;
+    }
+
+    return UNKNOWN;
+}
+
 void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
 {
     char platform[PROP_VALUE_MAX];
     char radio[PROP_VALUE_MAX];
     char device[PROP_VALUE_MAX];
-    char cdma_variant[92];
     char fstype[92];
     int rc;
 
@@ -80,12 +103,8 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
             property_set("persist.radio.multisim.config", "");
         }
     } else if (ISMATCH(radio, "0x3")) {
-        FILE *fp = popen("/system/bin/ls -la /fsg/falcon_3.img.gz | /system/bin/cut -d '_' -f3", "r");
-        fgets(cdma_variant, sizeof(cdma_variant), fp);
-        pclose(fp);
-        INFO("CDMA variant=%s", cdma_variant);
-        if (ISMATCH(cdma_variant, "verizon")) {
-            /* xt1028 */
+        switch (detect_carrier()) {
+        case VERIZON:
             property_set("ro.build.description", "falcon_verizon-user 5.1 LPB23.13-33.7 7 release-keys");
             property_set("ro.build.fingerprint", "motorola/falcon_verizon/falcon_cdma:5.1/LPB23.13-33.7/7:user/release-keys");
             property_set("ro.mot.build.customerid", "verizon");
@@ -94,13 +113,32 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
             property_set("ro.com.google.clientidbase.ms", "android-verizon");
             property_set("ro.com.google.clientidbase.am", "android-verizon");
             property_set("ro.com.google.clientidbase.yt", "android-verizon");
-        } else {
-            /* xt1031 */
+            break;
+        case SPRINT:
             property_set("ro.build.description", "falcon_boost-user 5.1 LPB23.13-56 55 release-keys");
             property_set("ro.build.fingerprint", "motorola/falcon_boost/falcon_cdma:5.1/LPB23.13-56/55:user/release-keys");
             property_set("ro.mot.build.customerid", "sprint");
-            property_set("ro.cdma.home.operator.alpha", "Boost Mobile");
-            property_set("ro.cdma.home.operator.numeric", "311870");
+            property_set("ro.cdma.home.operator.alpha", "Chameleon");
+            property_set("ro.cdma.home.operator.numeric", "310000");
+            property_set("ro.com.google.clientidbase.ms", "android-sprint-mvno-us");
+            property_set("ro.com.google.clientidbase.am", "android-sprint-mvno-us");
+            property_set("ro.com.google.clientidbase.yt", "android-sprint-mvno-us");
+            break;
+        case USC:
+            property_set("ro.build.description", "falcon_usc-user 5.1 LPB23.13-33.6 8 release-keys");
+            property_set("ro.build.fingerprint", "motorola/falcon_usc/falcon_cdma:5.1/LPB23.13-33.6/8:user/release-keys");
+            property_set("ro.mot.build.customerid", "usc");
+            property_set("ro.cdma.home.operator.numeric", "311220");
+            property_set("gsm.sim.operator.numeric", "311580");
+            property_set("ro.cdma.home.operator.alpha", "U.S. Cellular");
+            property_set("ro.com.google.clientidbase.ms", "android-uscellular-us");
+            property_set("ro.com.google.clientidbase.am", "android-uscellular-us");
+            property_set("ro.com.google.clientidbase", "android-motorola");
+            property_set("ro.com.google.clientidbase.gmm", "android-motorola");
+            property_set("ro.com.google.clientidbase.yt", "android-motorola");
+            break;
+        default:
+            ERROR("Unknown mobile carrier");
         }
         property_set("ro.product.device", "falcon_cdma");
         property_set("ro.build.product", "falcon_cdma");

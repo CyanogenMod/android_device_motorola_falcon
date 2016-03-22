e7c297e255334de0a2ce344ed8b4c06b2709f2b1
fonz93 <fonzino993@gmail.com>
2016-03-23 00:34:51 +0100

    falcon: build CMActions
    Change-Id: I3864d0494beb121f4c8fbdb8fc888d20e7ee8e51

--- device.mk
+++ device.mk
@@ -24,6 +24,10 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# CMActions
+PRODUCT_PACKAGES += \
+    CMActions
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.target.rc
